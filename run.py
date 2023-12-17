bf2309a9cc5ca6a8765bf7af90d2531a004ac8ed
Mbutler1991 <markbutlermb1991@gmail.com>
2023-12-17 00:22:33 +0000

    added pyfiglet to requirements.

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from termcolor import colored
 from tabulate import tabulate
+import pyfiglet
 
 # Scope for Google Sheets
 SCOPE = [
